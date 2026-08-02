9a36e6c2ab5daeee3890f9e73c43e4a936fa1ebd
jajhehb <narutokokoku@gmail.com>
2026-08-02 12:18:06 +0600

    Implement resolveAABB2D function for 2D collisions
    Added a new function to resolve 2D axis-aligned bounding box collisions.

--- src/core/physics/Collision.js
+++ src/core/physics/Collision.js
@@ -28,6 +28,37 @@ export function getOverlap(a, b) {
   return { overlapX, overlapY };
 }
 
+export function resolveAABB2D(a, b, posA, velA){
+  // calculate the overlap on both axis
+  const dx = Math.min(a.x + a.width, b.x + b.width) - Math.max(a.x,b.x); // formula
+  const dy =  Math.min(a.y + a.height, b.y + b.height) - Math.max(a.y, b.y); // same formula but for y axis
+
+  //1st if there is no actual overlap on either axis exit early
+
+  if (dx <= 0 || dy <= 0) return;
+
+  //2nd find centers to get the correct push direction
+  const centerA_x = a.x + a.width / 2;
+  const centerB_x = b.x + b.width / 2;
+  const centerA_y = a.y + a.height / 2;
+  const centerB_y = b.y + b.height / 2;
+
+  //3rd Push out along the SMALLEST overlap axis
+  if (dx <= dy){
+    // push Left or Right based on center positions
+    const sign = centerA_x < centerB_x ? -1 : 1;
+    posA.x += dx * sign;
+    a.x += dx * sign;  // Update the hitbox position immediately
+    if (velA) velA.vx = 0;
+  }
+  else{
+    // Push Up or Down based on center positions
+    const sign = centerA_y < centerB_y ? -1 : 1;
+    posA.y += dy * sign;
+    a.y += dy * sign; //Update the hitbox position immediately
+    if (velA) velA.vy = 0;
+  }
+}
 
 export function resolveAABB3D(a, b, posA, velA) {
   const dx =
