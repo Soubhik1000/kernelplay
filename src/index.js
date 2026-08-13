44a0dd7ffdd2d71268303069b3e7e37e8976a291
Soubhik <soubhik960@gmail.com>
2026-08-13 21:26:20 +0530

    Add Gamepad export to index.js

--- src/index.js
+++ src/index.js
@@ -3,6 +3,7 @@ export { Loop } from "./core/Loop.js";
 export { Time } from "./core/Time.js";
 export { Keyboard } from "./input/Keyboard.js";
 export { Mouse } from "./input/Mouse.js"
+export { Gamepad } from "./input/Gamepad.js"
 export { Canvas } from "./graphics/Canvas.js";
 export { Scene } from "./core/Scene.js";
 export { SceneManager } from "./core/SceneManager.js";
